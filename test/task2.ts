02e6f6a2b2e65e426afb4a31e3be499c74794923
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2025-02-19 18:46:53 +0200

    wait

--- test/task2.ts
+++ test/task2.ts
@@ -17,33 +17,33 @@ describe("Task 2", () => {
     // await driver.quit();
   });
 
-  // it("Task 2.1", async () => {
-  //   await driver
-  //     .findElement(
-  //       By.xpath("//div[contains(@class, 'top-card') and .//text()='Widgets']")
-  //     )
-  //     .click();
-  //   const progressBarItem = driver.findElement(
-  //     By.xpath("//span[text()='Progress Bar']")
-  //   );
-  //   await driver.executeScript(
-  //     "arguments[0].scrollIntoView()",
-  //     progressBarItem
-  //   );
-  //   await progressBarItem.click();
-  //   await driver.executeScript("scrollTo(0, 0)");
-  //   await driver.findElement(By.xpath("//button[text()='Start']")).click();
-  //   await driver.wait(
-  //     until.elementLocated(
-  //       By.xpath("//div[@role='progressbar' and @aria-valuenow='100']")
-  //     )
-  //   );
-  //   await driver.findElement(By.xpath("//button[text()='Reset']")).click();
-  //   const progressBarState = await driver
-  //     .findElement(By.xpath("//div[@role='progressbar']"))
-  //     .getText();
-  //   expect(progressBarState).to.equal("0%");
-  // });
+  it("Task 2.1", async () => {
+    await driver
+      .findElement(
+        By.xpath("//div[contains(@class, 'top-card') and .//text()='Widgets']")
+      )
+      .click();
+    const progressBarItem = driver.findElement(
+      By.xpath("//span[text()='Progress Bar']")
+    );
+    await driver.executeScript(
+      "arguments[0].scrollIntoView()",
+      progressBarItem
+    );
+    await progressBarItem.click();
+    await driver.executeScript("scrollTo(0, 0)");
+    await driver.findElement(By.xpath("//button[text()='Start']")).click();
+    await driver.wait(
+      until.elementLocated(
+        By.xpath("//div[@role='progressbar' and @aria-valuenow='100']")
+      )
+    );
+    await driver.findElement(By.xpath("//button[text()='Reset']")).click();
+    const progressBarState = await driver
+      .findElement(By.xpath("//div[@role='progressbar']"))
+      .getText();
+    expect(progressBarState).to.equal("0%");
+  });
 
   it("Task 2.2", async () => {
     await driver
@@ -52,33 +52,39 @@ describe("Task 2", () => {
       )
       .click();
     await driver.findElement(By.xpath("//span[text()='Web Tables']")).click();
-    while (true) {
-      const totalPages = await driver
-        .findElement(By.className("-totalPages"))
-        .getText();
-      if (totalPages === "2") break;
-      await driver.findElement(By.xpath("//button[text()='Add']")).click();
-      await driver
-        .findElement(By.xpath("//input[@placeholder='First Name']"))
-        .sendKeys("test");
-      await driver
-        .findElement(By.xpath("//input[@placeholder='Last Name']"))
-        .sendKeys("test");
-      await driver
-        .findElement(By.xpath("//input[@placeholder='name@example.com']"))
-        .sendKeys("test@test.test");
-      await driver
-        .findElement(By.xpath("//input[@placeholder='Age']"))
-        .sendKeys(0);
-      await driver
-        .findElement(By.xpath("//input[@placeholder='Salary']"))
-        .sendKeys(0);
-      await driver
-        .findElement(By.xpath("//input[@placeholder='Department']"))
-        .sendKeys("test");
-      await driver.findElement(By.xpath("//button[text()='Submit']")).click();
-      await driver.sleep(100);
-    }
+
+    await driver.wait(
+      async (driver) => {
+        await driver.findElement(By.xpath("//button[text()='Add']")).click();
+        await driver
+          .findElement(By.xpath("//input[@placeholder='First Name']"))
+          .sendKeys("test");
+        await driver
+          .findElement(By.xpath("//input[@placeholder='Last Name']"))
+          .sendKeys("test");
+        await driver
+          .findElement(By.xpath("//input[@placeholder='name@example.com']"))
+          .sendKeys("test@test.test");
+        await driver
+          .findElement(By.xpath("//input[@placeholder='Age']"))
+          .sendKeys(0);
+        await driver
+          .findElement(By.xpath("//input[@placeholder='Salary']"))
+          .sendKeys(0);
+        await driver
+          .findElement(By.xpath("//input[@placeholder='Department']"))
+          .sendKeys("test");
+        await driver.findElement(By.xpath("//button[text()='Submit']")).click();
+
+        const totalPages = await driver
+          .findElement(By.className("-totalPages"))
+          .getText();
+        return totalPages === "2";
+      },
+      undefined,
+      undefined,
+      200
+    );
     const nextButton = driver.findElement(By.xpath("//button[text()='Next']"));
     await driver.executeScript("arguments[0].scrollIntoView()", nextButton);
     await nextButton.click();
